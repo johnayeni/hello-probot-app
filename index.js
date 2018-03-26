08d47e8b3a7ac6046a1d145a44e790eb146dc571
johnayeni <john.ayeni@stu.cu.edu.ng>
2018-03-26 19:30:16 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = (robot) => {
+    robot.on('issues.opened', async context => {
+        const params = context.issue({ body: 'Hello World!' })
+        return context.github.issues.createComment(params)
+
+    })
+}
